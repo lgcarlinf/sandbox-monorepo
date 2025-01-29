4438516fb021a63b9fa128a16ff9d2d54336fdc0
luiggy <luiggylml@gmail.com>
2025-01-29 17:11:50 -0500

    update text

--- apps/nextjsv15/app/page.tsx
+++ apps/nextjsv15/app/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org -&gt; gaa
+          Go to nextjs.org
         </a>
       </footer>
     </div>
