c6ab9bb95ed12b348ab7c1c303db2b75e75f60e8
luiggy <luiggylml@gmail.com>
2025-01-29 14:15:41 -0500

    update package.lock

--- apps/nextjsv15/app/page.tsx
+++ apps/nextjsv15/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>app/page.tsx</code>.
+            Get started by editing <code> esta web</code>.
           </li>
           <li>Guarda tus cambios instantaneamente.</li>
         </ol>
